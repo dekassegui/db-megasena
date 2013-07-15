c3acac6ef848cadc95a058bdb9a48bfdbb954451
dekassegui <dekassegui@gmail.com>
2013-07-14 22:19:22 -0300

    Correção das hachuras dos gráficos via R...

--- R/plot-chi-one.r
+++ R/plot-chi-one.r
@@ -10,10 +10,16 @@ chi.tail <- function(x) { pchisq(x, chi.df, lower.tail=FALSE) }
 
 args = commandArgs(TRUE)
 estatistica = as.numeric(args[1])
-if (is.na(estatistica)) estatistica = chi.critical(0.5)
-
-#x11(display=":0.0", family='serif', 12.7, 6, 9)
-png(filename='chi-one.png', width=640, height=480, family='DejaVu Serif', pointsize=9)
+usingPNG = FALSE
+if (is.na(estatistica)) {
+  # renderização em ambiente interativo
+  estatistica = chi.critical(0.5)
+  X11(display=":0.0", family='serif', 12.7, 6, 9)
+} else {
+  # renderização em batch
+  png(filename='chi-one.png', width=640, height=480, family='DejaVu Serif', pointsize=9)
+  usingPNG = TRUE
+}
 
 opar=par(bg='white', fg='#333333')
 
@@ -36,39 +42,40 @@ cores <- c('#00cc00', '#cc0000')
 
 abline(v=valores, lty='solid', col=cores)
 
-gd <- par()$usr   # coordenadas dos extremos do device
+densidade = 25 # número de linhas por polegada nas hachuras
 
-legend(
-  2*gd[2]/3, 4*gd[4]/5,
-  bty='n',
-  cex=1.5,
-  text.font=2,      # bold
-  lty='solid',
-  col=cores,
-  legend=c(sprintf("%.3f (%5.3f)", valores, chi.tail(valores)))
-)
-
-# renderiza hachuras num intervalo arbitrário
+# renderiza hachuras no intervalo [from, to] e cor arbitrária
 hatch <- function(from, to, color) {
-  if (from < to) {
-    xx <- from
-    yy <- 0
-    a = from
-    while (a <= to) {
-      y = chi.density(a)
-      if (y < 1E-4) break
-      xx <- c(xx, a)
-      yy <- c(yy, y)
-      a = a + 0.03
-    }
-    xx <- c(xx, a)
+  xx = from
+  yy = 0
+  while (from < to) {
+    y = chi.density(from)
+    if (y < 1E-4) break
+    xx <- c(xx, from)
+    yy <- c(yy, y)
+    from = from + 0.03
+  }
+  if (length(xx) > 2) {
+    xx <- c(xx, from)
     yy <- c(yy, 0)
-    polygon(xx, yy, density=25, col=color, border=NA)
+    polygon(xx, yy, density=densidade, col=color, border=NA)
   }
 }
 
 hatch(valores[1], valores[2], cores[1])
 
+gd <- par()$usr   # coordenadas dos extremos do device
+
 hatch(valores[2], gd[2], cores[2])
 
-dev.off()
+legend(
+  2*gd[2]/3, 4*gd[4]/5,
+  bty='n',
+  cex=1.5,
+  text.font=2,  # bold
+  density=densidade,
+  fill=cores,
+  legend=c(sprintf("%.3f (%5.3f)", valores, chi.tail(valores)))
+)
+
+if (usingPNG) dev.off()
