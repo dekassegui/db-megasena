3f936f248f0a2ed2fc87b975be2ca639bd44ad1e
dekassegui <dekassegui@gmail.com>
2013-10-16 18:28:45 -0300

    Reprogramação da extensão 'Calendar'.

--- sqlite/calendar.c
+++ sqlite/calendar.c
@@ -36,7 +36,7 @@ static const int OFFSET[2][3] = { { 6, 3, 0 } /* DD-MM-YYYY */,
                                   { 0, 5, 8 } /* YYYY-MM-DD */ };
 
 /* Testa os valores dos componentes explícitos de alguma data. */
-static int chkdatefields(int year, int month, int day)
+static int chkdatefields(const int year, const int month, const int day)
 {
   char daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
@@ -51,24 +51,21 @@ static int chkdatefields(int year, int month, int day)
 /*
  * Validação dos componentes da data expressa na string 'date' terminada com
  * NUL, condicionada ao check-up sequencial do seu formato, conforme valor do
- * inteiro 'as_isodate'.
+ * inteiro 'x' (ZERO ou UM).
 */
-static int chkdate(const char *date, const int as_isodate)
+static int chkdate(const char *date, const int x)
 {
-  if (date) {
-    const int x = (as_isodate > 0) ? YYYY_MM_DD : DD_MM_YYYY;
-    int j, k = OFFSET[x][MONTH] - 1;
-    for (j = 0; j < k && IS_DIGIT(date[j]); ++j) ;
+  int j, k = OFFSET[x][MONTH] - 1;
+  for (j = 0; j < k && IS_DIGIT(date[j]); ++j) ;
+  if (j == k && IS_SEPARATOR(date[j])) {
+    k = OFFSET[x][(x == YYYY_MM_DD) ? DAY : YEAR] - 1;
+    for (j++; j < k && IS_DIGIT(date[j]); ++j) ;
     if (j == k && IS_SEPARATOR(date[j])) {
-      k = OFFSET[x][x ? DAY : YEAR] - 1;
-      for (j++; j < k && IS_DIGIT(date[j]); ++j) ;
-      if (j == k && IS_SEPARATOR(date[j])) {
-        for (k = 10, ++j; j < k && IS_DIGIT(date[j]); ++j) ;
-        if (j == k && date[j] == 0)
-          return chkdatefields(atoi(date + OFFSET[x][YEAR]),
-                               atoi(date + OFFSET[x][MONTH]),
-                               atoi(date + OFFSET[x][DAY]));
-      }
+      for (k = 10, ++j; j < k && IS_DIGIT(date[j]); ++j) ;
+      if (j == k && date[j] == 0)
+        return chkdatefields(atoi(date + OFFSET[x][YEAR]),
+                             atoi(date + OFFSET[x][MONTH]),
+                             atoi(date + OFFSET[x][DAY]));
     }
   }
   return 0;
@@ -110,20 +107,15 @@ static void chkdateFunc(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 /* Função complementar de "datefield" evitando código redundante. */
 static int chk_2nd_argument(sqlite3_context *ctx, int *f, sqlite3_value **argv)
 {
-  /* verifica se o segundo argumento é do tipo inteiro */
-  if (SQLITE_INTEGER != sqlite3_value_type(argv[1])) {
+  if (SQLITE_INTEGER == sqlite3_value_type(argv[1])) {
+    *f = sqlite3_value_int(argv[1]);
+    if (YEAR <= *f && *f <= DAY) return 1;
+    sqlite3_result_error(ctx, "número de ordem do componente é incorreto.\n" \
+      "Use 0 para extrair o ANO, 1 para extrair o mês e 2 para extrair o dia.", -1);
+  } else {
     sqlite3_result_error(ctx, "segundo argumento não é do tipo inteiro", -1);
-    return 0;
   }
-  *f = sqlite3_value_int(argv[1]);
-  /* verifica o número de ordem do componente a extrair */
-  if (*f < YEAR || *f > DAY) {
-    sqlite3_result_error(ctx, "número de ordem do componente é incorreto." \
-      "\nUse 0 para extrair o ANO, 1 para extrair o mês e 2 para extrair o " \
-      " dia.", -1);
-    return 0;
-  }
-  return 1;
+  return 0;
 }
 
 /*
@@ -137,17 +129,19 @@ static void datefield(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 {
   int f, rz;
 
-  if (SQLITE_INTEGER == sqlite3_value_type(argv[0]))
-  {
-    if (chk_2nd_argument(ctx, &f, argv) == 0) return ;
-    time_t seconds = (time_t) sqlite3_value_int64(argv[0]);
-    struct tm *time = localtime(&seconds);
-    if (YEAR == f) {
-      rz = time->tm_year + 1900;
-    } else if (MONTH == f) {
-      rz = time->tm_mon + 1;
+  if (SQLITE_INTEGER == sqlite3_value_type(argv[0])) {
+    if (chk_2nd_argument(ctx, &f, argv)) {
+      time_t seconds = (time_t) sqlite3_value_int64(argv[0]);
+      struct tm *broken_time = localtime(&seconds);
+      if (YEAR == f) {
+        rz = broken_time->tm_year + 1900;
+      } else if (MONTH == f) {
+        rz = broken_time->tm_mon + 1;
+      } else {
+        rz = broken_time->tm_mday;
+      }
     } else {
-      rz = time->tm_mday;
+      return ;
     }
   } else {
     char *date;
@@ -171,11 +165,11 @@ static void datefield(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 /*
  * Computa o unixtime da string de data no formato YYYY-MM-DD ou DD-MM-YYYY,
  * no instante ZERO do dia, relevando a "timezone" e "daylight saving time"
- * se disponíveis no sistema operacional.
+ * se disponíveis no sistema operacional. O primeiro argumento é a data e o
+ * segundo é o número de ordem do seu formato conforme "enum date_formats".
 */
-static time_t datestring_to_nixtime(char *date)
+static time_t datestring_to_nixtime(const char *date, const int x)
 {
-  const enum date_formats x = IS_SEPARATOR(date[4]) ? YYYY_MM_DD : DD_MM_YYYY;
   time_t curtime = time(NULL);
   struct tm *broken_time = localtime(&curtime);
   broken_time->tm_year = atoi(date + OFFSET[x][YEAR]) - 1900;
@@ -190,12 +184,13 @@ static time_t datestring_to_nixtime(char *date)
  * ou "unixtime"; da data contida no argumento de tipo string com formato
  * YYYY-MM-DD ou DD-MM-YYYY.
 */
-static void nixtime(sqlite3_context *ctx, int argc, sqlite3_value **argv)
+static void timestamp(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 {
   if (SQLITE3_TEXT == sqlite3_value_type(argv[0])) {
     char *date = (char *) sqlite3_value_text(argv[0]);
-    if (chkdate(date, YYYY_MM_DD) || chkdate(date, DD_MM_YYYY)) {
-      sqlite3_result_int64(ctx, (long int) datestring_to_nixtime(date));
+    int x = chkdate(date, YYYY_MM_DD);
+    if (x || chkdate(date, DD_MM_YYYY)) {
+      sqlite3_result_int64(ctx, (long int) datestring_to_nixtime(date, x));
     } else {
       sqlite3_result_error(ctx, "argumento não contém data valida", -1);
     }
@@ -206,16 +201,38 @@ static void nixtime(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 
 /*
  * Retorna string de data no formato DD-MM-YYYY ou YYYY-MM-DD, sendo o primeiro
- * argumento o seu número de segundos na era Unix, seguido de valor zero-um que
- * indica o formato desejado, armazenando o resultado no buffer apontado pelo
- * último argumento, que ao menos deve conter 11 bytes.
+ * argumento o seu número de segundos na era Unix, seguido de valor ZERO-UM que
+ * indica o formato desejado.
 */
-static char *nixtime_to_datestring(time_t seconds, int as_isodate, char *buf)
+static char *nixtime_to_datestring(const time_t seconds, const int as_isodate)
 {
-  struct tm *time = localtime(&seconds);
-  const char *format = (as_isodate == YYYY_MM_DD) ? "%Y-%m-%d" : "%d-%m-%Y";
-  (void) strftime(buf, 11, format, time);
-  return buf;
+  struct tm *broken_time = localtime(&seconds);
+  if (as_isodate) {
+    return sqlite3_mprintf("%04d-%02d-%02d", broken_time->tm_year+1900, \
+             broken_time->tm_mon+1, broken_time->tm_mday);
+  }
+  return sqlite3_mprintf("%02d-%02d-%04d", broken_time->tm_mday, \
+           broken_time->tm_mon+1, broken_time->tm_year+1900);
+}
+
+#define TIMESTAMP_0000_01_01_00_00_BRT -62167208012L
+
+#define TIMESTAMP_9999_12_31_00_00_BRT 253402225200L
+
+/*
+ * Checa a magnitude do timestamp a formatar como DD-MM-YYYY ou YYYY-MM-DD,
+ * notificando a limitação natural como erro.
+*/
+static int chk_timestamp(sqlite3_context *ctx, const time_t seconds)
+{
+  if (seconds < TIMESTAMP_0000_01_01_00_00_BRT) {
+    sqlite3_result_error(ctx, "data resultante é anterior a 01-01-0000.", -1);
+  } else if (seconds > TIMESTAMP_9999_12_31_00_00_BRT) {
+    sqlite3_result_error(ctx, "data resultante é posterior a 31-12-9999.", -1);
+  } else {
+    return 1;
+  }
+  return 0;
 }
 
 /*
@@ -247,10 +264,10 @@ static void datestr(sqlite3_context *ctx, int argc, sqlite3_value **argv)
     as_isodate = (sqlite3_value_int(argv[1]) > 0) ? DD_MM_YYYY : YYYY_MM_DD;
   }
   seconds = (time_t) sqlite3_value_int64(argv[0]);
-  date = (char *) sqlite3_malloc(11);
-  nixtime_to_datestring(seconds, as_isodate, date);
-  sqlite3_result_text(ctx, date, -1, SQLITE_TRANSIENT);
-  sqlite3_free(date);
+  if (chk_timestamp(ctx, seconds)) {
+    date = nixtime_to_datestring(seconds, as_isodate);
+    sqlite3_result_text(ctx, date, -1, sqlite3_free);
+  }
 }
 
 #define WORD(ptr) *((unsigned short int *) (ptr))
@@ -293,13 +310,14 @@ static void days_between_dates(ctx, argc, argv)
 {
   time_t seconds[2];
   char *date;
-  int j;
+  int j, x;
 
   for (j = 0; j < 2; ++j) {
     if (SQLITE3_TEXT == sqlite3_value_type(argv[j])) {
       date = (char *) sqlite3_value_text(argv[j]);
-      if (chkdate(date, YYYY_MM_DD) || chkdate(date, DD_MM_YYYY)) {
-        seconds[j] = datestring_to_nixtime(date);
+      x = chkdate(date, YYYY_MM_DD);
+      if (x || chkdate(date, DD_MM_YYYY)) {
+        seconds[j] = datestring_to_nixtime(date, x);
       } else {
         char *z = sqlite3_mprintf("argumento #%d nao contém data valida", j+1);
         sqlite3_result_error(ctx, z, -1);
@@ -321,22 +339,23 @@ static void days_between_dates(ctx, argc, argv)
 static void weekday(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 {
   const char *WEEKDAY[7] = { "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb" };
-  struct tm *time;
+  struct tm *broken_time;
   time_t seconds;
 
   if (SQLITE_INTEGER == sqlite3_value_type(argv[0])) {
     seconds = (time_t) sqlite3_value_int64(argv[0]);
   } else if (SQLITE3_TEXT == sqlite3_value_type(argv[0])) {
     char *date = (char *) sqlite3_value_text(argv[0]);
-    if (chkdate(date, YYYY_MM_DD) || chkdate(date, DD_MM_YYYY)) {
-      seconds = datestring_to_nixtime(date);
+    int x = chkdate(date, YYYY_MM_DD);
+    if (x || chkdate(date, DD_MM_YYYY)) {
+      seconds = datestring_to_nixtime(date, x);
     } else {
       sqlite3_result_error(ctx, "argumento não contém data valida", -1);
       return ;
     }
   }
-  time = localtime(&seconds);
-  sqlite3_result_text(ctx, WEEKDAY[time->tm_wday], -1, SQLITE_TRANSIENT);
+  broken_time = localtime(&seconds);
+  sqlite3_result_text(ctx, WEEKDAY[broken_time->tm_wday], -1, SQLITE_TRANSIENT);
 }
 
 /*
@@ -346,6 +365,8 @@ static void weekday(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 */
 static void today(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 {
+  char *date;
+  time_t seconds;
   int as_isodate = YYYY_MM_DD;
 
   if (argc > 1) {
@@ -361,11 +382,9 @@ static void today(sqlite3_context *ctx, int argc, sqlite3_value **argv)
       return ;
     }
   }
-  char *date = (char *) sqlite3_malloc(11);
-  time_t seconds = time(NULL);
-  nixtime_to_datestring(seconds, as_isodate, date);
-  return sqlite3_result_text(ctx, date, -1, SQLITE_TRANSIENT);
-  sqlite3_free(date);
+  seconds = time(NULL);
+  date = nixtime_to_datestring(seconds, as_isodate);
+  sqlite3_result_text(ctx, date, -1, sqlite3_free);
 }
 
 /*
@@ -376,8 +395,8 @@ static void today(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 static void dateadd(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 {
   char *date;
-  long int seconds, ndays;
-  int x = YYYY_MM_DD;
+  long int seconds;
+  int ndays, x = YYYY_MM_DD;
 
   if (SQLITE_INTEGER == sqlite3_value_type(argv[0])) {
     seconds = sqlite3_value_int64(argv[0]);
@@ -392,21 +411,23 @@ static void dateadd(sqlite3_context *ctx, int argc, sqlite3_value **argv)
       sqlite3_result_error(ctx, "primeiro argumento não contém data valida", -1);
       return ;
     }
-    seconds = (long int) datestring_to_nixtime(date);
+    seconds = (long int) datestring_to_nixtime(date, x);
   }
   if (SQLITE_INTEGER != sqlite3_value_type(argv[1])) {
     sqlite3_result_error(ctx, "segundo argumento não é do tipo inteiro", -1);
     return ;
   }
   ndays = sqlite3_value_int(argv[1]);
-  seconds += ndays * 86400;
-  date = (char *) sqlite3_malloc(11);
-  nixtime_to_datestring((time_t) seconds, x, date);
-  sqlite3_result_text(ctx, date, -1, SQLITE_TRANSIENT);
-  sqlite3_free(date);
+  seconds += ndays * 86400L;
+  if (chk_timestamp(ctx, seconds)) {
+    date = nixtime_to_datestring((time_t) seconds, x);
+    sqlite3_result_text(ctx, date, -1, sqlite3_free);
+  }
 }
 
 #define FAST_ABS(x) (((x) ^ ((x) >> 31)) - ((x) >> 31))
+
+extern long int timezone;
 
 /*
  * Informa a configuração da "timezone" e "daylight saving time" quando
@@ -414,20 +435,18 @@ static void dateadd(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 */
 static void timezone_info(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 {
-  char *z;
-  int h, m;
   time_t seconds = time(NULL);
   struct tm *broken_time = localtime(&seconds);
-  h = FAST_ABS(timezone);
-  m = h % 3600 / 60;
-  h /= 3600;
-  z = sqlite3_mprintf("%s\nUTC%c%02d:%02d", broken_time->tm_zone, \
-                      (((timezone > 0) << 1) + '+'), h, m);
+  int h = FAST_ABS(timezone);
+  int m = h % 3600 / 60;
+  char *z = sqlite3_mprintf("UTC%c%02d:%02d", ((timezone > 0) ? '-' : '+'), h/3600, m);
+#if __GNUC__ && !__STRICT_ANSI__
+  z = sqlite3_mprintf("%s\n%z", broken_time->tm_zone, z);
+#endif
   if (broken_time->tm_isdst) {
     z = sqlite3_mprintf("%z\nObservação: Usando horário de verão.", z);
   }
-  sqlite3_result_text(ctx, z, -1, SQLITE_TRANSIENT);
-  sqlite3_free(z);
+  sqlite3_result_text(ctx, z, -1, sqlite3_free);
 }
 
 int sqlite3_extension_init(db, err, api)
@@ -437,7 +456,7 @@ int sqlite3_extension_init(db, err, api)
 
   sqlite3_create_function(db, "CHKDATE", -1, SQLITE_UTF8, NULL, chkdateFunc, NULL, NULL);
   sqlite3_create_function(db, "DATEPART", 2, SQLITE_UTF8, NULL, datefield, NULL, NULL);
-  sqlite3_create_function(db, "TIMESTAMP", 1, SQLITE_UTF8, NULL, nixtime, NULL, NULL);
+  sqlite3_create_function(db, "TIMESTAMP", 1, SQLITE_UTF8, NULL, timestamp, NULL, NULL);
   sqlite3_create_function(db, "DATESTR", -1, SQLITE_UTF8, NULL, datestr, NULL, NULL);
   sqlite3_create_function(db, "SWAPFORMAT", 1, SQLITE_UTF8, NULL, swap_format, NULL, NULL);
   sqlite3_create_function(db, "DIFFDATES", 2, SQLITE_UTF8, NULL, days_between_dates, NULL, NULL);
