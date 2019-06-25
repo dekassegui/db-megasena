cb7f45b74c6bf44239a2aaa102c14ce6138f89e4
Antonio <dekassegui@gmail.com>
2019-06-25 00:42:01 -0300

    Simplificação de consulta de reincidentes.

--- sql/conta-reincidentes.sql
+++ sql/conta-reincidentes.sql
@@ -1,19 +1,13 @@
-CREATE TEMP TABLE reincidentes AS
-  SELECT
-    concursos.concurso,
-    (SELECT dezenas FROM dezenas_juntadas
-     WHERE concurso == concursos.concurso)
-     & (SELECT dezenas FROM dezenas_juntadas
-        WHERE concurso == concursos.concurso-1) AS R
-  FROM
-    concursos
-  WHERE
-    (concursos.concurso > 1) AND (R != 0);
+-- tabela dos números sorteados em concursos consecutivos
+create temp view reincidentes as
+  select concurso, dezena from dezenas_sorteadas as s
+  where (
+    select 1 from dezenas_sorteadas
+    where concurso == s.concurso-1 and dezena == s.dezena);
 
-CREATE TEMP VIEW v AS
-  SELECT concurso, MASK60(R) AS mask FROM reincidentes;
-
-CREATE TEMP VIEW w AS
-  SELECT concurso, LENGTH(REPLACE(mask, '0', '')) AS len FROM v;
-
-SELECT len, COUNT(*) FROM w GROUP BY len;
+-- contagem de concursos pela quantidade de números sorteados em concursos
+-- consecutivos
+select n, count(concurso) from (
+  -- tabela da quantidade de números sorteados em concursos consecutivos
+  select concurso, count(dezena) as n from reincidentes group by concurso
+) group by n;
